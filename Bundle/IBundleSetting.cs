2884871e810b1999c6eae14315a28fafa0e25492
xunyihuang <k820720k.cute@gmail.com>
2023-03-04 20:42:01 +0800

    Modify naming format

--- Bundle/IBundleSetting.cs
+++ Bundle/IBundleSetting.cs
@@ -1,13 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Builder.Bundle
 {
     public interface IBundleSetting
     {
-        void SetBundleId(string appId);
-        void SetBundleVersion(string version, int versionCode);
-        void SetKeystore(string pass, string keyName, string keyPass);
+        void SetBundleId(string inAppId);
+        void SetBundleVersion(string inVersion, int inVersionCode);
+        void SetKeystore(string inPass, string inKeyName, string inKeyPass);
     }
 }
